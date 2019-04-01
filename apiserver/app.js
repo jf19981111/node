b9e7bc38dc61d4c6dd3ff8f1a6b9c3d3657d096a
jf981111 <983446103u@qq.com>
2019-04-01 19:11:44 +0800

    上传文件的demo

--- apiserver/app.js
+++ apiserver/app.js
@@ -1,53 +1,143 @@
 const express = require('express');
 const path = require('path');
-const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const multer = require('multer');
+const upload = multer({dest:'c/tmp'}) //设置上传文件的临时路径
+// 返回的是一个处理 upload 的对象
+/**
+ * // 都返回的是一个中间件函数
+ * // 单个文件
+ * upload.single('filed') //处理单个文件
+ * // 指定一个字段多个文件
+ * upload.array('filed',maxCount) // 处理多个文件 abc -> 多个文件
+ * 
+ * upload.fileds([
+ *      {
+ *          name : 'filed' , // 字段名
+ *          maxCount : 2 // 控制这个字段名所能上传的文件的个数
+ *      },
+ *      {
+ *          name : 'efg',
+ *          maxCount : 3
+ *      }
+ * ])
+ * // 可以指定多个字段，多个文件
+ * 
+ * 上面这三个函数调用之后 返回的是中间件函数，也就是说带有 req.res，next 参数的那个中间件函数，对 req.body 做了处理,
+ * 可以拿到除了上传文件信息的其他文本信息，还增加了 req.file || req.files 都是一个文件信息的对象
+ * 
+ */
 const app = express();
 
-const userRouter = require('./routes/userRouter');
-
-//设置对 req.body 的使用
-//默认只对 json 格式 或者 普通表单格式 的数据做解析 Form Data
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended : false}));
 
-//设置静态资源托管文件夹
-app.use(express.static(path.resolve(__dirname,'./public')))
+app.use(express.static(path.resolve(__dirname,'./public')));
 
-// 允许 所有的请求跨越
-app.use((req,res,next) => {
-    res.set('Access-Control-Allow-Origin',"*")
-    res.set('Access-Control-Allow-Headers',"content-type,X-Access-Token")
-    next();
+// 上传多个文件 upload.fileds
+app.post('/upload',upload.fields([
+    {name : 'abc',maxCount : 2},
+    {name : 'efg',maxCount : 2}
+]),(req,res) => {
+    console.log(req.body)
+    console.log(req.files);
+    /**
+     * {
+        abc:
+        [ { fieldname: 'abc',
+            originalname: 'timg (1).jpg',
+            encoding: '7bit',
+            mimetype: 'image/jpeg',
+            destination: 'c/tmp',
+            filename: 'c48ab95f6b2e5ec579c94396acbf7ca3',
+            path: 'c\\tmp\\c48ab95f6b2e5ec579c94396acbf7ca3',
+            size: 53271 } ],
+        efg:
+        [ { fieldname: 'efg',
+            originalname: 'timg.jpg',
+            encoding: '7bit',
+            mimetype: 'image/jpeg',
+            destination: 'c/tmp',
+            filename: '7a13046ee38f80145d217fc265713749',
+            path: 'c\\tmp\\7a13046ee38f80145d217fc265713749',
+            size: 52852 } ] 
+        }
+
+     * 
+     * 
+     * 
+     */
+
+ /*    let filesUrl = []
+
+    for(var i = 0; i < req.files.length; i++) {
+        let curFile = req.files[i];
+
+        //需要对当前的文件 做转存 存到 静态资源托管的文件夹中
+        let newFileName = new Date().getTime() + '_' + curFile.originalname;
+        let newFilePath = path.resolve(__dirname,'./public',newFileName)
+        let fileData = fs.readFileSync(curFile.path);
+        fs.writeFileSync(newFilePath,fileData);
+
+        filesUrl.push(`http://localhost:3000/${ newFileName}`)
+    }
+
+    res.send({
+        code : 0,
+        msg : '上传文件成功',
+        // imgUrl : 'http://localhost:3000/' + newFileName
+        filesUrl : filesUrl
+    }) */
 })
 
-//验证 token 
-//=============  注意不能再这里使用 中间件 因为 所有的路由都会在这里经过处理 而登录不需要处理  ============
-/* app.use((req,res,next) => {
-    // 0. 获取前端在请求头中传递过来的 X-Access-Token
-    let token = req.get('X-Access-Token')
-    // 1. 是否存在
-    if (!token) {//不存在,那么直接返回没有数据
-        res.status(401).send({code: -1,msg : 'token不存在'})
-    } else {//存在
-        // 2. 验证 token 的有效性
-        jwt.verify(token,'keyt',(err,payload) => {
-            // 判断 验证是否 成功
-            if (err) {
-                //验证失败
-                res.status(403).send({code: -1,msg : '没有权限'})
-            } else {
-                //验证成功
-                //我们可以像别的中间件一样，在req 身上加上一个属性
-                //取出 payload 中的 userId
-                req.userInfo = payload;
-                next();
-            }
-        })
+
+/* // 上传 多个文件 Array 使用
+app.post('/upload',upload.array('abc',2),(req,res) => {
+    console.log(req.body)
+    console.log(req.files);
+
+    let filesUrl = []
+
+    for(var i = 0; i < req.files.length; i++) {
+        let curFile = req.files[i];
+
+        //需要对当前的文件 做转存 存到 静态资源托管的文件夹中
+        let newFileName = new Date().getTime() + '_' + curFile.originalname;
+        let newFilePath = path.resolve(__dirname,'./public',newFileName)
+        let fileData = fs.readFileSync(curFile.path);
+        fs.writeFileSync(newFilePath,fileData);
+
+        filesUrl.push(`http://localhost:3000/${ newFileName}`)
     }
+
+    res.send({
+        code : 0,
+        msg : '上传文件成功',
+        // imgUrl : 'http://localhost:3000/' + newFileName
+        filesUrl : filesUrl
+    })
 }) */
 
 
 
-app.use('/api',userRouter);
+/* // 上传单个文件
+app.post('/upload',upload.single('abc'),(req,res) => {
+    console.log(req.body)
+    console.log(req.file)
+//需要对当前的文件 做转存 存到 静态资源托管的文件夹中
+    let newFileName = new Date().getTime() + '_' + req.file.originalname;
+    let newFilePath = path.resolve(__dirname,'./public',newFileName)
+    let fileData = fs.readFileSync(req.file.path);
+    fs.writeFileSync(newFilePath,fileData);
+
+    res.send({
+        code : 0,
+        msg : '上传文件成功',
+        imgUrl : 'http://localhost:3000/' + newFileName
+    })
+}) */
 
 app.listen(3000);
+
+//要做文件的上传
+//用到 multer 模块
--- apiserver/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/project', {useNewUrlParser : true})
-    .then(() => { console.log('数据库连接成功') })
-    .catch(() => { console.log('数据库连接失败') })
-
-module.exports = mongoose;
